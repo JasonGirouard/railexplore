23aaafb7add2a41681ca2b4690e3a346404df5f9
JasonGirouard <jasontgirouard@gmail.com>
2024-03-12 20:08:24 -0400

    modifying python process on st out with a try catch

--- server.js
+++ server.js
@@ -8,36 +8,38 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'build')));
 
 // API endpoint for path calculation
-app.get('/api/paths/:origin/:destination', async (req, res) => {
-    const { origin, destination } = req.params;
-  
-    // Validate the origin and destination parameters
-    if (!origin || !destination || origin.length !== 3 || destination.length !== 3) {
-      return res.status(400).json({ error: 'Invalid origin or destination' });
-    }
-  
-    // Execute the Python script
-    const pythonProcess = await spawn('python3', ['scripts/path_calculation.py', origin, destination]);
-  
-    // opt 1 - wait for this to finish, then write this to a file and then read it. 
-    // the problem is that the pythonProcess doesn't get a result. I need to need for pythonProcess.stdout to finish before finishing the rest. 
-    let pythonOutput = [];
-    pythonProcess.stdout.on('data', (data) => {
-      console.log('pythonOutput1: ',data)
-      pythonOutput += data.toString();
-    });
-  
-    pythonProcess.on('close', (code) => {
+app.get('/api/paths/:origin/:destination', (req, res) => {
+  const { origin, destination } = req.params;
 
-      
-      console.log('pythonOutput2: ',code)
-      console.log('pythonOutput3: ',res)
-      console.log('pythonOutput4:', pythonOutput)
-      console.log('pythonOutput5:',JSON.parse(pythonOutput) )
-  
-      res.json(JSON.parse(pythonOutput));
-    });
+  // Validate the origin and destination parameters
+  if (!origin || !destination || origin.length !== 3 || destination.length !== 3) {
+    return res.status(400).json({ error: 'Invalid origin or destination' });
+  }
+
+  // Execute the Python script
+  const pythonProcess = spawn('python3', ['scripts/path_calculation.py', origin, destination]);
+
+  let pythonOutput = '';
+  pythonProcess.stdout.on('data', (data) => {
+    console.log('pythonOutput1: ', data);
+    pythonOutput += data.toString();
   });
+
+  pythonProcess.on('close', (code) => {
+    console.log('pythonOutput2: ', code);
+    console.log('pythonOutput3: ', res);
+    console.log('pythonOutput4:', pythonOutput);
+
+    try {
+      const parsedOutput = JSON.parse(pythonOutput);
+      console.log('pythonOutput5:', parsedOutput);
+      res.json(parsedOutput);
+    } catch (error) {
+      console.error('Error parsing Python output:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });
+});
 
 // Catch-all route to serve the React app
 app.get('*', (req, res) => {
