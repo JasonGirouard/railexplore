e65ace8ee33fa6b656fe1e780084ffab74fe401a
JasonGirouard <jasontgirouard@gmail.com>
2024-03-15 21:11:42 -0400

    testing

--- src/Explore.jsx
+++ src/Explore.jsx
@@ -8,31 +8,29 @@ import stations from "./data/stations.json";
 import LocationDetector from "./LocationDetector";
 import Search from "./Search"; // Adjust the path as needed
 
-const Explore = ({
-  originStation,
-  setOriginStation,
-}) => {
-    const defaultStation = {
-        name: "New York, NY",
-        is_recommended: true,
-        code: "NYP",
-        city: "New York",
-        state: "NY",
-        address1: "351 West 31st Street",
-        zipcode: "10001",
-        lat: "40.7503352640001",
-        long: "-73.9944604469999",
-        Shelter: true,
-        station_type: "Station Building",
-        mode: "TRAIN",
-        description: "",
-      };
+const Explore = ({ originStation, setOriginStation }) => {
+  const defaultStation = {
+    name: "New York, NY",
+    is_recommended: true,
+    code: "NYP",
+    city: "New York",
+    state: "NY",
+    address1: "351 West 31st Street",
+    zipcode: "10001",
+    lat: "40.7503352640001",
+    long: "-73.9944604469999",
+    Shelter: true,
+    station_type: "Station Building",
+    mode: "TRAIN",
+    description: "",
+  };
 
-    const [isPanelOpen, setIsPanelOpen] = useState(false);
-    const [selectedStationDestinations, setSelectedStationDestinations] = useState(null);
-    const [activeStation, setActiveStation] = useState(defaultStation);
+  const [isPanelOpen, setIsPanelOpen] = useState(false);
+  const [selectedStationDestinations, setSelectedStationDestinations] =
+    useState(null);
+  const [activeStation, setActiveStation] = useState(defaultStation);
 
- // this is necessary to re-render the map
+  // this is necessary to re-render the map
   useEffect(() => {
     setSelectedStationDestinations(
       stationSummary.find(
@@ -49,24 +47,24 @@ const Explore = ({
         stations={stations}
         originStation={originStation}
         setOriginStation={setOriginStation}
-       
       />
 
-      <MapComponent
-        originStation={originStation}
-        setIsPanelOpen={setIsPanelOpen}
-        activeStation={activeStation}
-        setActiveStation={setActiveStation}
-        selectedStationDestinations={selectedStationDestinations}
-      />
-
-      <InfoPanel
-        originStation={originStation}
-        station={activeStation}
-        isPanelOpen={isPanelOpen}
-        setIsPanelOpen={setIsPanelOpen}
-        selectedStationDestinations={selectedStationDestinations} 
-      />
+      <div className="map-container">
+        <MapComponent
+          originStation={originStation}
+          setIsPanelOpen={setIsPanelOpen}
+          activeStation={activeStation}
+          setActiveStation={setActiveStation}
+          selectedStationDestinations={selectedStationDestinations}
+        />
+        <InfoPanel
+          originStation={originStation}
+          station={activeStation}
+          isPanelOpen={isPanelOpen}
+          setIsPanelOpen={setIsPanelOpen}
+          selectedStationDestinations={selectedStationDestinations}
+        />
+      </div>
     </div>
   );
 };
