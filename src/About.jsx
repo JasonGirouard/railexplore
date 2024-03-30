85b39310de64119f88c8207a3cec0dc96a9b1518
JasonGirouard <jasontgirouard@gmail.com>
2024-03-30 18:01:00 -0400

    adding fathom analytics

--- src/About.jsx
+++ src/About.jsx
@@ -7,73 +7,74 @@ import TopNav from "./TopNav";
 const About = ({ setActivePage }) => {
   return (
     <div>
-            <TopNav  setActivePage={setActivePage}/>
-    
-    <div className="about-page">
-      <div className="about-container">
-        <div className="about-text">
-          <h4>About</h4>
-          <h1>Travel by Train.</h1>
-          <p className="travel-by-train-desc">
-            Train Gang exists to help you travel by train. At best, it hopes to
-            inspire you to visit local destinations you otherwise wouldn't.
-          </p>
-          <p>
-            Passenger rail provides a peace of mind, free from traffic and
-            dangerous drivers, that traveling by car simply can't deliver. Why
-            not travel by train?
-          </p>
-        </div>
-        <div className="about-image">
-          <img src={womanReadingImage} alt="Woman Reading" />
-        </div>
-      </div>
+      <TopNav setActivePage={setActivePage} />
 
-      <div className="value-section">
-        <div className="value-row">
-          <div className="value-column">
-            <h2>Built to find you a destination.</h2>
+      <div className="about-page">
+        <div className="about-container">
+          <div className="about-text">
+            <h4>About</h4>
+            <h1>Travel by Train.</h1>
+            <p className="travel-by-train-desc">
+              Train Gang exists to help you travel by train. At best, it hopes
+              to inspire you to visit local destinations you otherwise wouldn't.
+            </p>
+            <p>
+              Passenger rail provides a peace of mind, free from traffic and
+              dangerous drivers, that traveling by car simply can't deliver. Why
+              not travel by train?
+            </p>
           </div>
-          <div className="value-column">
-            <p>
-              Amtrak and other passenger rail websites are effective when you
-              already know your origin & destination — but they aren't effective
-              when you don't.
-            </p>
-            <p>
-              Rather than staring at transit maps and manually checking
-              different travel times & prices, why not explore all your
-              possibilities at once?
-            </p>
+          <div className="about-image">
+            <img src={womanReadingImage} alt="Woman Reading" />
           </div>
         </div>
 
-        <div className="value-divider"></div>
-
-        <div className="value-row">
-          <div className="value-column">
-            <div className = "trainy">
-              <img src={Trainy2} alt="Train Logo" />
+        <div className="value-section">
+          <div className="value-row">
+            <div className="value-column">
+              <h2>Built to find you a destination.</h2>
             </div>
-            
-            <p>
-  This site is free to use, but not free to operate. Please{" "}
-  <Link
-    to="/donate"
-    style={{ textDecoration: "underline", color: "inherit" }}
-  >
-    donate
-  </Link>{" "}
-  if you can.
-</p>
+            <div className="value-column">
+              <p>
+                Amtrak and other passenger rail websites are effective when you
+                already know your origin & destination — but they aren't
+                effective when you don't.
+              </p>
+              <p>
+                Rather than staring at transit maps and manually checking
+                different travel times & prices, why not explore all your
+                possibilities at once?
+              </p>
+            </div>
           </div>
-          <div className="value-column">
-            <h2>Please donate if you can.</h2>
+
+          <div className="value-divider"></div>
+
+          <div className="value-row">
+            <div className="value-column">
+              <div className="trainy">
+                <img src={Trainy2} alt="Train Logo" />
+              </div>
+
+              <p>
+                This site is free to use, but not free to operate. Please{" "}
+                <Link
+                to="https://www.buymeacoffee.com/jasontgirom"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ textDecoration: "underline", color: "inherit" }}
+              >
+                  donate
+                </Link>{" "}
+                if you can.
+              </p>
+            </div>
+            <div className="value-column">
+              <h2>Please donate if you can.</h2>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-
     </div>
   );
 };
--- src/Donate.jsx
+++ src/Donate.jsx
@@ -1,9 +1,32 @@
 import React from "react";
+import Trainy2 from "./images/Trainy2.png";
+import { Link } from "react-router-dom";
+import TopNav from "./TopNav";
+import "./About.css";
 
 const Donate = ({ activePage, setActivePage }) => {
- 
   return (
-    <div>Donate Page</div>
+    <div>
+      <TopNav setActivePage={setActivePage} />
+
+      <div className="donate-page">
+        <div className="donate-container">
+          <div className="donate-column info">
+            <p>This site is free to use but not free to operate. Please consider donating if you can.</p>
+          </div>
+          <div className="donate-column coffee">
+            <iframe
+              src="https://www.buymeacoffee.com/widget/page/jasontgirom"
+              width="100%"
+              height="500px"
+              frameBorder="0"
+              allowTransparency="true"
+              style={{ border: "none", width: "100%", height: "500px" }}
+            ></iframe>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
