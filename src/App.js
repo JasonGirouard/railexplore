8030f8852186ab4d5c05b096fdf80a6149cd1565
JasonGirouard <jasontgirouard@gmail.com>
2024-04-22 11:03:38 -0600

    testing new styling

--- src/App.js
+++ src/App.js
@@ -51,6 +51,20 @@ function App() {
     enableAutoPageviews();
   }, []);
 
+  useEffect(() => {
+    const updateVh = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    };
+  
+    updateVh();
+    window.addEventListener('resize', updateVh);
+  
+    return () => {
+      window.removeEventListener('resize', updateVh);
+    };
+  }, []);
+
   
 
   
