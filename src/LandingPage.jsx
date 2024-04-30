0756ab36833eb2c3cbc2dc7b6acdf8c7e5f458a2
JasonGirouard <jasontgirouard@gmail.com>
2024-04-30 19:03:47 -0400

    landing page improvement

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import "./LandingPage.css";
 import Trainy2 from "./images/Trainy2.png";
-import LandingPageMap from "./images/landingPageMap.png";
+import LandingPageMap from "./images/landingPageMap2.png";
 
 const LandingPage = () => {
   return (
     <div className="landing-page">
       <div className="landing-page-container">
         <div className="landing-page-text">
-          {/* <img
+          <img
             src={Trainy2}
             className="landing-page-logo"
             alt="landing page map"
-          /> */}
+          />
           <h1>Travel by Train.</h1>
           <p className="travel-by-train-desc">
             Enter a city or address to see everywhere you can travel by train.
